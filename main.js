14dbbe512d2276b687d799005c1b2f6c78c6057d
Chao Zhou <Czhou0k@gmail.com>
2022-02-24 10:42:52 -0500

    animejs is working

---
+++ main.js
@@ -0,0 +1,23 @@
+anime({
+  targets: '.square',
+  translateX: 1000,
+  duration: 5000,
+  // loop: true,
+});
+
+anime({
+  targets: '.circle',
+  translateX: 1000,
+  duration: 5000,
+  backgroundColor: '#00FF00',
+  // loop: true,
+});
+
+anime({
+  targets: '#triangle',
+  translateX: 1000,
+  rotate: '5turn',
+  scaleX: 3,
+  duration: 5000,
+  loop: true,
+});
