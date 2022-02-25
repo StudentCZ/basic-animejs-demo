fb296bc2949dcbb8eee89d858cee75d2eabf0e6e
Chao Zhou <Czhou0k@gmail.com>
2022-02-24 19:15:04 -0500

    made a lot of animations, pretty cool

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ anime({
   targets: '.square',
   translateX: 1000,
   duration: 5000,
-  // loop: true,
+  delay: 1000,
+  loop: true,
 });
 
 anime({
@@ -10,7 +11,8 @@ anime({
   translateX: 1000,
   duration: 5000,
   backgroundColor: '#00FF00',
-  // loop: true,
+  delay: 2000,
+  loop: true,
 });
 
 anime({
@@ -19,5 +21,37 @@ anime({
   rotate: '5turn',
   scaleX: 3,
   duration: 5000,
+  endDelay: 100,
+  direction: 'alternate',
+  loop: true,
+});
+
+anime({
+  targets: '.square1',
+  translateX: {
+    value: 500,
+    duration: 1000,
+  },
+  rotate: {
+    value: 1000,
+    duration: 2000,
+    easing: 'easeInOutSine',
+  },
+  scale: {
+    value: 4,
+    duration: 2000,
+    delay: 500,
+    easing: 'easeInOutQuart',
+  },
+  delay: 300,
+  direction: 'alternate',
+  loop: true,
+});
+
+anime({
+  targets: '.triangle-down',
+  translateY: 200,
+  delay: 500,
+  direction: 'alternate',
   loop: true,
 });
